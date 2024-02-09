b789680c681d23791f8254cd2e5809b85e9cfb89
aaromarc327 <aaromarc327@SDS-L-763HC63>
2024-02-09 14:04:10 -0500

    Add project files.

---
+++ GameButton/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GameButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.BorderSize = 3;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Forte", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Lime;
+            this.startButton.Location = new System.Drawing.Point(296, 307);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(208, 90);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Font = new System.Drawing.Font("Forte", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.Lime;
+            this.countdownLabel.Location = new System.Drawing.Point(41, 91);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(706, 117);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "Ready";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countdownLabel.Click += new System.EventHandler(this.countdownLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Game Button";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ GameButton/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace GameButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void countdownLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.countdownSound);
+            player.Play();
+
+            Refresh();
+            Thread.Sleep(300);
+
+            countdownLabel.Text = "Starting in 3...";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "Starting in 2...";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "Starting in 1...";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            SoundPlayer player2 = new SoundPlayer(Properties.Resources.endSound);
+            player2.Play();
+
+            this.BackColor = Color.Green;
+            countdownLabel.ForeColor = Color.Black;
+            countdownLabel.Text = "Go";
+        }
+    }
+}
