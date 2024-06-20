d4128aeaecf5ecf73a152ed596be9a0d2452b7b4
aaromarc327 <aaromarc327@SDS-L-763HC63>
2024-06-20 14:07:22 -0400

    Added Win screen if you cover entire screen

--- GameButton/Form1.cs
+++ GameButton/Form1.cs
@@ -55,6 +55,14 @@ namespace GameButton
             this.BackColor = Color.Green;
             countdownLabel.ForeColor = Color.Black;
             countdownLabel.Text = "Go";
+
+            Refresh();
+            Thread.Sleep(10000);
+
+            startButton.Visible = true;
+            this.BackColor = Color.Black;
+            countdownLabel.Text = "Ready"
+            countdownLabel.ForeColor = Color.Lime;
         }
     }
 }
